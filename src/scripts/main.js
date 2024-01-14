e099864136266c88b85a04873da8f955ef38a050
Wanderson Gabriel <gabriel_wanderson@hotmail.com>
2024-01-14 20:16:57 -0300

    Início de implentação da Lógica de tempo restante.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
-console.log ("Olá mundo!");
+const dataDoAniversario = new Date ("Sep 04, 2024 05:15:00");
+const timeStampDataDoAniversario = dataDoAniversario.getTime();
+
+console.log ("Hora em TimeStamp: "+ timeStampDataDoAniversario);
+
+const calculaTempoEmTimeStamp = setInterval(function(){
+    const horaAtual = new Date();
+    const timeStampHoraAtual = horaAtual.getTime();
+
+    const conversaoMilissegundosParaMinutos = 1000 * 60;
+    const conversaoMilissegundosParaHoras = 1000 * 60 * 60;
+    const conversaoMilissegundosParaDias = 1000 * 60 * 60 * 24;
+
+    const calculaDiferencaEntreDataAtualDataEvento = timeStampDataDoAniversario - timeStampHoraAtual;
+},1000)
